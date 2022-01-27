4481f4559ee8e19a268d0d86357be8c87cc1fe50
Alsoexistnot <76039531+Alsoexistnot@users.noreply.github.com>
2022-01-27 20:41:59 +0300

    Add files via upload

---
+++ geek-spring-part-one-16-11/lesson-05-hibernate/src/main/java/ru/geekbrains/entity/Customer.java
@@ -0,0 +1,46 @@
+package ru.geekbrains.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    @OneToMany(
+            mappedBy = "customer",
+            orphanRemoval = true
+    )
+    private List<LineItem> lineItems;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<LineItem> getLineItems() {
+        return lineItems;
+    }
+
+    public void setLineItems(List<LineItem> lineItems) {
+        this.lineItems = lineItems;
+    }
+}
---
+++ geek-spring-part-one-16-11/lesson-05-hibernate/src/main/java/ru/geekbrains/entity/Product.java
@@ -0,0 +1,58 @@
+package ru.geekbrains.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private BigDecimal price;
+
+    @OneToMany(
+            mappedBy = "product",
+            orphanRemoval = true
+    )
+    private List<LineItem> lineItems;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public List<LineItem> getLineItems() {
+        return lineItems;
+    }
+
+    public void setLineItems(List<LineItem> lineItems) {
+        this.lineItems = lineItems;
+    }
+}
